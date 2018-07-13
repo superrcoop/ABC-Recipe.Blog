8b7fe8aabb644feb0c95f78df257bebf2d235d18
Jordan Cooper <jordan.cooper3000@gmail.com>
2018-07-13 11:40:55 -0500

    Update schema.sql

--- Template-vuejs/schema.sql
+++ Template-vuejs/schema.sql
@@ -21,13 +21,6 @@ create table IF NOT EXISTS Profile(
 	primary key(profile_id)
 ;
 
-drop table if exists Ingredients;
-create table IF NOT EXISTS Ingredients(
-	ingredients_id int auto_increment not null,
-	name varchar(100) not null,
-	primary key(ingredients_id)
-);
-
 drop table if exists Uploads;
 create table IF NOT EXISTS Uploads(
 	user_name varchar(100) not null,
@@ -49,35 +42,13 @@ create table IF NOT EXISTS Instructions(
 drop table if exists Recipe;
 create table IF NOT EXISTS Recipe(
 	recipe_id int auto_increment not null,
-	name varchar(100) not null,
-	cook_time varchar(100) not null,
+	title varchar(100) not null,
+	description varchar(100) not null,
+	ingredients varchar(300) not null,
 	prep_time varchar(100) not null,
-	diet_type varchar(100) not null,
-	primary key(recipe_id)
-);
-
-drop table if exists Meal;
-create table IF NOT EXISTS Meal(
-	meal_id int auto_increment not null,
-	name varchar(100) not null,
-	meal_type varchar(100) not null,
+	recipe_type varchar(100) not null,
 	img_src varchar(255) not null,
-	primary key(meal_id)
-);
-drop table if exists Creates;
-create table IF NOT EXISTS Creates(
-	recipe_id int not null,
-	meal_id int not null,
-	foreign key(recipe_id) references Recipe(recipe_id) on delete cascade on update cascade,
-	foreign key(meal_id) references Meal(meal_id) on delete cascade on update cascade,
-	primary key(recipe_id, meal_id)
-);
-
-drop table if exists Consists;
-create table IF NOT EXISTS Consists(
-	ingredients_id int not null,
-	measure_id int not null,
-	foreign key(ingredients_id) references Ingredients(ingredients_id) on delete cascade on update cascade,
-	foreign key(measure_id) references Measurements(measure_id) on delete cascade on update cascade,
-	primary key(ingredients_id, measure_id)
+	instruction_id int not null,
+	foreign key(instruction_id) references Instructions(instruction_id) on delete cascade on update cascade,
+	primary key(recipe_id)
 );
